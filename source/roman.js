376c9a5a733b331f294fe16639f3184f2fed0282
Georgiy <dev.georgiy@gmail.com>
2021-08-30 23:13:15 +0300

    add roman function

---
+++ source/roman.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function roman(inputNum) {
+    let inputType = typeof(inputType);
+    if (inputType != "string" && inputType != "number") {
+        throw TypeError("Неверный тип входных данных!")
+    }
+
+}
